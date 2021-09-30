2c4b6f743dc80db25f086b61c8fdd0854e11b50e
Unknown <mm0x0b@gmail.com>
2021-10-01 00:03:12 +0900

    ファイルを追加

---
+++ ATT.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ATT
+{
+#if UNITY_IOS
+    private const string DLL_NAME = "__Internal";
+
+    // ATT承認状態を取得（同期）
+    [DllImport (DLL_NAME)]
+    private static extern int _GetATTStatus ();
+    public static int GetATTStatus ()
+    {
+        if (Application.isEditor) {
+            return -1;
+        }
+        return _GetATTStatus ();
+    }
+
+    // ATT承認を要求（非同期）
+    [DllImport (DLL_NAME)]
+    private static extern void _RequestATT (OnCompleteCallback callback);
+
+    private delegate void OnCompleteCallback (int status);
+    private static SynchronizationContext _context;
+    private static UnityAction <int> _OnComplete;
+
+    public static void RequestATT (UnityAction <int> OnComplete)
+    {
+        if (Application.isEditor) {
+            // 呼出元のActionの引数を0にして実行する
+            OnComplete?.Invoke (0);
+            return;
+        }
+#if UNITY_IOS
+        _context = SynchronizationContext.Current;
+        _OnComplete = OnComplete;
+        _RequestATT (OnRequestComplete);
+#endif
+    }
+
+    // ATT要求完了時のコールバック
+    [AOT.MonoPInvokeCallback (typeof (OnCompleteCallback))]
+    private static void OnRequestComplete (int status)
+    {
+        if (_OnComplete != null) {
+            _context.Post(_ => {
+                _OnComplete?.Invoke (status);
+                _OnComplete = null;
+            }, null);
+        }
+    }
+#endif
+}
---
+++ Main.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Main : MonoBehaviour
+{
+    void Awake ()
+    {
+        // iOSのときのみATT操作を実行
+#if UNITY_IOS
+        ManageATT ();
+#endif
+    }
+
+    // ATT操作を実行
+    void ManageATT ()
+    {
+        int status = ATT.GetATTStatus ();
+
+        // ATT未設定のとき
+        if (status == 0)
+            // リクエストダイアログを表示
+            ATT.RequestATT (CompleteATT);
+        else
+        {
+            // ATT設定不可
+            if (status == 1 || status == 2) {
+                CantManageATT ();
+                return;
+            }
+
+            // 設定済み
+            AlreadyAnswerdATT ();
+        }
+    }
+
+    // 回答済みのとき実行
+    void AlreadyAnswerdATT ()
+    {
+        Debug.Log ("設定済み");
+    }
+
+    // ユーザー回答後に実行
+    void CompleteATT (int status)
+    {
+        Debug.Log ("設定完了");
+    }
+
+    // 設定不可のとき実行
+    void CantManageATT ()
+    {
+        Debug.Log ("設定不可");
+    }
+}
